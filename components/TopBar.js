20a34ec53c9be5467e2d222906ac1279a1d5b108
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2021-05-12 16:58:48 +0200

    ee

---
+++ components/TopBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import {FontAwesome5, FontAwesome} from '@expo/vector-icons'
+export default function 
+
+
+() {
+  return (
+    <View style={styles.container}>
+      <FontAwesome5  name="fire" size={27} color="#F06795"></FontAwesome5>
+      <Text>
+
+
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({container: {
+  height: 60,
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  padding: 15,
+  backgroundColor: 'white',
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 10,},
+    shadowOpacity: 0.12,
+    shadowRadius: 5.46,
+    elevation: 9,},
+});
